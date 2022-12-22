b1d27e9172eb012a63b028e90a07fa8b2c23da21
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-22 17:13:41 +0100

    added firebase url to change password and useref hook

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,16 +1,45 @@
+import {useRef, useContext} from "react";
+
 import classes from './ProfileForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    // maybe: add validation
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAG7y-kKvDaGz5Maq_1y68N80DQYkG2DPI',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            password: enteredNewPassword,
+            returnSecureToken: false
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(res => {
+          //assumption: always successful
+
+    })
+  };
+
   return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='new-password'>New Password</label>
+          <input type='password' id='new-password' ref={newPasswordInputRef} minLength='7'/>
+        </div>
+        <div className={classes.action}>
+          <button>Change Password</button>
+        </div>
+      </form>
   );
 }
 
