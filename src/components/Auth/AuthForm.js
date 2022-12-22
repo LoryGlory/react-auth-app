b8a8c029f7ef43ac5ae6b8cf21463d77db21f77a
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-22 16:16:54 +0100

    added function to login users after successful authentication

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -20,39 +20,48 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
 
     if (isLogin) {
-// ...
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAG7y-kKvDaGz5Maq_1y68N80DQYkG2DPI';
     } else {
-      fetch(
-          'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAG7y-kKvDaGz5Maq_1y68N80DQYkG2DPI',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              email: enteredEmail,
-              password: enteredPassword,
-              returnSecureToken: true
-            }),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }
-      ).then((res) => {
-        setIsLoading(false);
-
-        if (res.ok) {
-          // ...
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = 'Authentication failed';
-            // if (data && data.error && data.error.message) {
-            //   errorMessage = data.error.message;
-            // }
-            alert(errorMessage);
-          });
-        }
-      });
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAG7y-kKvDaGz5Maq_1y68N80DQYkG2DPI';
     }
+
+    fetch(
+        url,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+    ).then((res) => {
+      setIsLoading(false);
+
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = 'Authentication failed';
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          alert(errorMessage);
+          throw new Error(errorMessage);
+        });
+      }
+    }).then((data) => {
+      console.log(data);
+    })
+        .catch((err) => {
+          alert(err.message);
+        });
   };
 
   return (
